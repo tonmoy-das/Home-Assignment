e2ee32637bf759017dae617169c4afa6b814f0bf
Tonmoy Das <you@example.comtonmoydas555@gmail.com>
2024-05-14 15:15:37 +0600

    code refactor

--- task_3/script.js
+++ task_3/script.js
@@ -1,10 +1,12 @@
+const userMenu = document.getElementById("user-menu");
+const profilePictureBtn = document.getElementById("profile-picture-dropdown");
+
 // Function to toggle the visibility of the user menu
+
 function toggleUserMenu() {
-  const userMenu = document.getElementById("user-menu");
   userMenu.classList.toggle("hidden");
 }
 
 // Add click event listener to the profile dropdown
-document
-  .getElementById("profile-picture-dropdown")
-  .addEventListener("click", toggleUserMenu);
+
+profilePictureBtn.addEventListener("click", toggleUserMenu);
